11ebe63ac72c5819e8b6a988e9d5ef123e642fff
Zapharaos <mfreitag@numericable.fr>
2023-02-01 10:01:25 +0100

    to components (raw)

---
+++ src/components/AppDashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <section-presentation/>
+  <section-aboutme/>
+  <section-experience/>
+  <section-formation/>
+  <section-project/>
+  <div id="curriculum">
+    <p>For your reference, here is my curriculum.</p>
+    <form action="./assets/files/curriculum.pdf" target="_blank">
+      <button><i class="fa fa-download"></i>Download</button>
+    </form>
+  </div>
+  <section-contact/>
+</template>
+
+<script>
+import SectionPresentation from './SectionPresentation.vue'
+import SectionAboutme from './SectionAboutme.vue'
+import SectionExperience from './SectionExperience.vue'
+import SectionFormation from './SectionFormation.vue'
+import SectionProject from './SectionProject.vue'
+import SectionContact from './SectionContact.vue'
+
+export default {
+  name: 'AppDashboard',
+  components: {
+    SectionPresentation,
+    SectionAboutme,
+    SectionExperience,
+    SectionFormation,
+    SectionProject,
+    SectionContact
+  }
+}
+</script>
---
+++ src/components/SectionContact.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="section-contact">
+    <h3>Contact</h3>
+    <form id="form-contact">
+      <div>
+        <input type="text" id="name" name="name" placeholder="Name" tabindex="1" required>
+        <input type="email" id="email" name="email" placeholder="Email" tabindex="2" required>
+      </div>
+      <input type="text" id="subject" name="subject" placeholder="Subject" tabindex="3" required>
+      <textarea rows="5" id="message" name="message" placeholder="Message" tabindex="4" required></textarea>
+      <button type="submit" name="submit" value="Submit" tabindex="5">Submit</button>
+      <input type="hidden" name="spam">
+    </form>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'SectionContact',
+  }
+</script>
