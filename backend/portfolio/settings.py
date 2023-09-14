8259510434e7f56ae6644b8e81e24b4e96a66450
Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
2023-09-14 14:29:35 +0200

    3 emails (#24)
    * email ready & working
    
    * form controls front/back
    
    * fixes : docker django vuejs & contact

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -39,16 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djongo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -90,25 +91,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -134,3 +116,21 @@ STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cors
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # Replace with your frontend URL
+    "http://vuejs:8080",  # If your frontend is running in another Docker container
+]
+
+# Emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ['CONTACT_EMAIL_HOST']
+EMAIL_PORT = os.environ['CONTACT_EMAIL_PORT']
+EMAIL_HOST_USER = os.environ['CONTACT_EMAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['CONTACT_EMAIL_PASSWORD']
