0ae09ad0c388a455a639751832d938ab67ad3b57
Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
2024-03-17 19:24:02 +0100

    #32 restart vuejs (#37)
    * cleaning up after missing elements from #34
    
    * restarting vue js project

--- frontend/public/assets/scripts/variables.js
+++
@@ -1,6 +0,0 @@
-// Contact
-const idFormContact = 'form-contact'
-const idCloseNotifications = "close-notifications"
-const selectorNotification = '#notification'
-const selectorMessage = '#notification p'
-const selectorIcon = '#notification i.fa-2x'
