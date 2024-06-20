73b496dd0a4d5549fc1cf261e7a84deef355e168
Matthieu FREITAG <56632861+Zapharaos@users.noreply.github.com>
2024-06-20 17:23:51 +0200

    #41 call api (#42)
    * calling user API
    
    * user service unit tests + CI
    
    * fixing vitest CI script
    
    * fixing vite codecov plugin
    
    * cleaning up cypress tests
    
    * basic cypress test

---
+++ src/types/models.ts
@@ -0,0 +1,42 @@
+export interface Theme {
+  name: string
+  todo: string
+}
+
+export interface User {
+  id: number
+  hero: string
+  description: string
+  email: string
+  logo: string
+  photo?: string
+  curriculum?: string
+  theme_light?: Theme
+  theme_dark?: Theme
+  socials: Social[]
+  lists: List[]
+}
+
+export interface Social {
+  name: string
+  url: string
+  hidden: boolean
+}
+
+export interface List {
+  name: string
+  index: number
+  hidden: boolean
+  items: ListItem[]
+}
+
+export interface ListItem {
+  index: number
+  organisation: string
+  name: string
+  menuName?: string
+  address?: string
+  period?: string
+  description?: string
+  hidden: boolean
+}
