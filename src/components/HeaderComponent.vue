ac0671b148dcc470ea8802c7670c602c6910f344
Zapharaos <56632861+Zapharaos@users.noreply.github.com>
2024-06-25 00:24:30 +0200

    feat: header responsive menu + component comments

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,36 +1,89 @@
 <script lang="ts">
-let lastScrollTop = 0;
 export default {
+  data() {
+    return {
+      lastScrollTop: 0, // Tracks the last scroll position for header visibility
+      showResponsiveMenu: false, // Flag to indicate if responsive menu is open
+    }
+  },
   mounted() {
-    const headerLinks = document.querySelectorAll('header a');
-    headerLinks.forEach(link => {
+    // Add event listener for scroll events
+    window.addEventListener('scroll', this.handleScroll);
+
+    // Add click event listeners to all header links
+    document.querySelectorAll('header a').forEach(link => {
       link.addEventListener('click', this.scrollToSection);
     });
-    window.addEventListener('scroll', this.handleScroll);
   },
   methods: {
+    /**
+     * Prevents default link behavior and scrolls to the target section smoothly.
+     * Also closes the responsive menu if it's open.
+     *
+     * @param event The click event object triggered on a link.
+     */
     scrollToSection(event: any) {
+      // Prevent default link behavior
       event.preventDefault();
-      const targetId = event.target.dataset.target; // Get the data-target ID
-      const section = document.getElementById(targetId);
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
+
+      // Close responsive menu if it's open
+      if (this.showResponsiveMenu) {
+        this.toggleResponsiveMenu();
       }
+
+      // Get the target section ID from the data attribute
+      const targetId = event.target.dataset.target;
+
+      // Scroll to the target section smoothly
+      document.getElementById(targetId)?.scrollIntoView({ behavior: 'smooth' });
     },
+    /**
+     * Handles scroll events to show/hide the header based on scroll direction.
+     * Ignores scrolls while the responsive menu is active.
+     */
     handleScroll() {
+      // Ignore scrolls when responsive menu is active
+      if(this.showResponsiveMenu) return;
+
+      // Get the current scroll position (credits: https://github.com/qeremy/so/blob/master/so.dom.js#L426)
+      const st = document.documentElement.scrollTop;
+
       const header = document.querySelector('header');
-      const st = document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+      // Check if there's a header element
       if (!header) {
         return;
-      } else if (st > lastScrollTop) {
+      }
+
+      // Hide header on scroll down, show on scroll up
+      if (st > this.lastScrollTop) {
         header.style.opacity = "0";
         header.style.visibility = "hidden";
-      } else if (st < lastScrollTop) {
+      } else if (st < this.lastScrollTop) {
         header.style.opacity = "1";
         header.style.visibility = "visible";
-      } // else was horizontal scroll
-      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    }
+      }
+
+      // Reset lastScrollTop for mobile or negative scrolling scenarios
+      this.lastScrollTop = st <= 0 ? 0 : st;
+    },
+    /**
+     * Toggles the visibility of the responsive menu and applies necessary style changes.
+     */
+    toggleResponsiveMenu() {
+      // Toggle the responsive menu state
+      this.showResponsiveMenu = !this.showResponsiveMenu;
+
+      // Toggle display class on menu SVGs
+      document.querySelectorAll('.responsive-menu-svg').forEach(svg => {
+        svg.classList.toggle('display');
+      });
+
+      // Toggle display class for the menu list
+      document.querySelector('ul')?.classList.toggle('display');
+
+      // Toggle body class to disable/enable scroll while menu is open
+      document.body.classList.toggle('responsive-menu');
+    },
   },
 };
 </script>
@@ -41,6 +94,10 @@ export default {
       <a class="logo">
         <img src="https://matthieu-freitag.com/img/nav-logo.png" alt="Logo"/>
       </a>
+      <button aria-label="menu" class="responsive-menu-btn" @click="toggleResponsiveMenu">
+        <svg class="responsive-menu-svg display" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px"><path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"/></svg>
+        <svg class="responsive-menu-svg close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
+      </button>
       <ul>
         <li>
           <a data-target="work">
@@ -85,6 +142,7 @@ nav {
 .logo {
   display: flex;
   align-items: center;
+  z-index: 9999;
 }
 .logo img {
   height: 1.5rem;
@@ -108,26 +166,80 @@ li a:hover {
 a:hover {
   cursor: pointer;
 }
-@media (max-width: 768px) {
-  header {
-    font-size: 1.25rem;
-  }
+</style>
+
+<style scoped>
+/* Responsive styles */
+.responsive-menu-btn {
+  background: unset;
+  color: unset;
+  pointer-events: auto;
+  display: none;
+  position: relative;
+  border: 0;
+  padding: 0;
+  margin: 0;
+  z-index: 9999;
+  cursor: pointer;
+}
+svg {
+  visibility: hidden;
+  color: white;
+  stroke: white;
+  fill: white;
+  scale: 0;
+  transition: all 0.3s ease-in-out;
+  width: 24px;
+  height: 24px;
+}
+.responsive-menu-btn svg:not(:first-child) {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.display {
+  visibility: inherit;
+  opacity: 1;
+}
+svg.display {
+  transition-delay: 0.2s;
+  scale: initial;
+}
+@media only screen and (max-width: 768px) {
   nav {
     padding: 1rem 2rem;
   }
-  li:not(:first-child) {
-    margin-left: 1.5rem;
+  .responsive-menu-btn {
+    display: flex;
+  }
+  ul {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    max-height: 100svh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: stretch;
+    flex-direction: column;
+    padding: 3rem 1rem;
+    grid-gap: unset;
+    overflow-y: auto;
+    visibility: hidden;
+    opacity: 0;
+    background: var(--color-background);
+  }
+  ul li {
+    margin: 0 !important;
+    font-size: 4rem;
+    text-align: center;
   }
 }
 @media (max-width: 576px) {
-  header {
-    font-size: 1.125rem;
-  }
   nav {
     padding: 1rem 1.5rem;
-  }
-  li:not(:first-child) {
-    margin-left: 1.25rem;
   }
 }
 </style>
