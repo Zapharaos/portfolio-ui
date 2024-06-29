34a5d0a205f506dc7b54a35a30441ce13497a2d5
Zapharaos <56632861+Zapharaos@users.noreply.github.com>
2024-06-30 01:15:22 +0200

    feat: project card to link with animation

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,22 +3,29 @@ const props = defineProps(['project'])
 </script>
 
 <template>
-  <li class="project-card">
-    <h3>{{ props.project.title }}</h3>
-    <img v-if="props.project.image" :src="props.project.image" :alt="props.project.title"/>
-    <p>{{ props.project.description }}</p>
-    <ul class="technologies">
-      <li v-for="technology in props.project.technologies" :key="technology" class="technologies-tag">
-        {{ technology }}
-      </li>
-    </ul>
+  <li>
+    <a :href="props.project.url" target="_blank" class="project-card {{ props.project.url ? 'clickable' : '' }}">
+      <h3>
+        {{ props.project.title }}
+        <span v-if="props.project.url">
+          <svg xmlns="http://www.w3.org/2000/svg" class="link-open" width="24" height="24"><path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/></svg>
+        </span>
+      </h3>
+      <img v-if="props.project.image" :src="props.project.image" :alt="props.project.title"/>
+      <p>{{ props.project.description }}</p>
+      <ul class="technologies">
+        <li v-for="technology in props.project.technologies" :key="technology" class="technologies-tag">
+          {{ technology }}
+        </li>
+      </ul>
+    </a>
   </li>
 </template>
 
 <style scoped>
 .project-card {
-  /*background-color: #484848;*/
   background-color: #2F2516;
+  border: solid 3px transparent;
   border-radius: 10px;
   padding: 2rem;
   color: white;
@@ -26,15 +33,21 @@ const props = defineProps(['project'])
   flex-direction: column;
   justify-content: center;
   gap: 1rem;
+  text-decoration: none;
 }
 h3 {
   font-size: 2rem;
+}
+.link-open {
+  visibility: hidden;
+  fill: orange;
+  opacity: 0;
 }
 img {
   max-width: 100%;
   margin: 5%;
   border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  box-shadow: 0 0 25px 25px rgba(93, 63, 17, 0.375);
 }
 p {
   text-align: justify;
@@ -55,5 +68,20 @@ p {
   font-size: .75rem;
   font-weight: 500;
   padding: 0.25rem 0.75rem;
+}
+
+.project-card, .link-open, img {
+  transition: all 0.5s ease-in-out;
+}
+.project-card:hover {
+  background-color: #423214;
+  border-color: #573E11;
+}
+.project-card:hover .link-open {
+  visibility: initial;
+  opacity: 1;
+}
+.project-card:hover img{
+  box-shadow: 0 0 25px 25px rgba(93, 63, 17, 0.75);
 }
 </style>
