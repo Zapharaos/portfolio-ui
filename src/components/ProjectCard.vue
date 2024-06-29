29b526930a3c699225d314c840d391198fa5da1a
Zapharaos <56632861+Zapharaos@users.noreply.github.com>
2024-06-29 22:20:40 +0200

    feat: project: responsive, img, tags

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,7 +5,7 @@ const props = defineProps(['project'])
 <template>
   <li class="project-card">
     <h3>{{ props.project.title }}</h3>
-    <img :src="props.project.image" :alt="props.project.title"/>
+    <img v-if="props.project.image" :src="props.project.image" :alt="props.project.title"/>
     <p>{{ props.project.description }}</p>
     <ul class="technologies">
       <li v-for="technology in props.project.technologies" :key="technology" class="technologies-tag">
@@ -17,7 +17,8 @@ const props = defineProps(['project'])
 
 <style scoped>
 .project-card {
-  background-color: #484848;
+  /*background-color: #484848;*/
+  background-color: #2F2516;
   border-radius: 10px;
   padding: 2rem;
   color: white;
@@ -29,27 +30,30 @@ const props = defineProps(['project'])
 h3 {
   font-size: 2rem;
 }
+img {
+  max-width: 100%;
+  margin: 5%;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
 p {
   text-align: justify;
 }
 .technologies {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   gap: 0.5rem;
   list-style: none;
   margin: 0;
   padding: 0;
 }
 .technologies-tag {
-  padding: 5px;
-  border-radius: 5px;
-  border: 1px solid orange;
+  border-radius: 9999px;
   color: orange;
-}
-
-@media (max-width: 400px) {
-  .project-card {
-    padding: 1rem;
-  }
+  background-color: #5D3F11;
+  font-size: .75rem;
+  font-weight: 500;
+  padding: 0.25rem 0.75rem;
 }
 </style>
