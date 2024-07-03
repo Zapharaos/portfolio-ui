a29004126f9eb0d2f86e3d72630a920402366d59
Zapharaos <matthieu.freitag@gmail.com>
2024-07-03 22:33:31 +0200

    feat: about section setup

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+// Define the props for the component
+const props = defineProps<{
+  text: string
+}>()
+</script>
+
+<template>
+  <section id="about">
+    <h2>About me</h2>
+    <div class="grid-container">
+      <img class="manga" src="/src/assets/file.png" alt="manga"/>
+      <p>
+        {{ text }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem
+}
+.grid-container {
+  grid-template-columns: 1fr 2fr;
+  gap: 5rem;
+}
+.manga {
+  border: solid 0.25rem white;
+  border-radius: 1rem;
+  width: inherit;
+}
+p {
+  color: white;
+  font-size: 1.125rem;
+  text-align: justify;
+  align-self: center;
+}
+</style>
