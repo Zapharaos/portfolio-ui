96bdd075d682b9c41a260b39f433feb163559645
Zapharaos <matthieu.freitag@gmail.com>
2024-07-10 01:24:54 +0200

    feat: build ui: script setup 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,140 +1,145 @@
-<script lang="ts">
+<script setup lang="ts">
+import {ref, reactive, onMounted, computed} from 'vue';
+import { useRouter } from 'vue-router';
 
-  const gridWidth = 5; // Number of puzzle pieces per row
-  const gridHeight = 3; // Number of puzzle rows
+interface PuzzlePiece {
+  style: Record<string, string>; // Style properties for the puzzle piece
+  correctIndex: number; // The correct index of the piece in the puzzle
+}
 
-  // Since a square piece is required, calculate the smallest size based on the window dimensions.
-  const additionalUnits = 4;
+const gridWidth = 5; // Number of puzzle pieces per row
+const gridHeight = 3; // Number of puzzle rows
 
-  function calculatePieceSize() {
-    let currentUnits = additionalUnits
-    if (window.innerWidth <= 576) {
-      currentUnits = 1;
-    }
-    else if (window.innerWidth <= 992) {
-      currentUnits = 2;
-    }
-    return Math.min(window.innerWidth / (gridWidth + currentUnits), window.innerHeight / (gridHeight + currentUnits));
+const router = useRouter();
+
+/**
+ * Calculates the size of each puzzle piece based on the window size.
+ *
+ * @returns The size of each puzzle piece in pixels.
+ */
+const calculatePieceSize = () => {
+  let units = 4;
+  if (window.innerWidth <= 576) {
+    units = 1;
+  } else if (window.innerWidth <= 992) {
+    units = 2;
   }
+  return Math.min(window.innerWidth / (gridWidth + units), window.innerHeight / (gridHeight + units));
+}
 
-  // Interface for a Puzzle Piece
-  interface PuzzlePiece {
-    style: Record<string, string>; // Style properties for the puzzle piece
-    correctIndex: number; // The correct index of the piece in the puzzle
-  }
+const pieceSize = ref(calculatePieceSize());
+const puzzlePieces = reactive<PuzzlePiece[]>([]);
+const dragIndex = ref(null as number | null);
+const correctlyPlacedCount = ref(0);
+const totalPieces = ref(gridWidth * gridHeight);
 
-  export default {
-    data() {
-      return {
-        pieceSize: calculatePieceSize(),
-        puzzlePieces: [] as PuzzlePiece[], // Array of puzzle pieces
-        dragIndex: null as number | null, // Index of the currently dragged piece
-        correctlyPlacedCount: 0, // Number of correctly placed pieces
-        totalPieces: gridWidth * gridHeight, // Total number of puzzle pieces
-      };
-    },
-    computed: {
-      puzzleContainerStyle() {
-        return {
-          gridTemplateColumns: `repeat(${gridWidth}, ${this.pieceSize}px)`,
-          gridTemplateRows: `repeat(${gridHeight}, ${this.pieceSize}px)`,
-        };
-      },
-    },
-    methods: {
-      /**
-       Redirect to home page
-       */
-      redirect(){
-        this.$router.push({ name: 'PortfolioView' });
-      },
-      /**
-       * Prepares an array of puzzle pieces with their styles and correct positions.
-       */
-      preparePuzzlePieces() {
-        let pieces: PuzzlePiece[] = [];
-
-        for (let row = 0; row < gridHeight; row++) {
-          for (let col = 0; col < gridWidth; col++) {
-            const index = row * gridWidth + col;
-            pieces.push({
-              style: {
-                backgroundImage: 'url(/src/assets/404.svg)',
-                backgroundSize: `${gridWidth * this.pieceSize}px ${gridHeight * this.pieceSize}px`,
-                backgroundPosition: `-${col * this.pieceSize}px -${row * this.pieceSize}px`,
-                width: `${this.pieceSize}px`,
-                height: `${this.pieceSize}px`,
-              },
-              correctIndex: index,
-            });
-          }
-        }
-        return pieces;
-      },
-      /**
-       * Shuffles an array using the Fisher-Yates shuffle algorithm.
-       * @param array The array to shuffle.
-       * @returns The shuffled array.
-       */
-      shuffleArray(array: PuzzlePiece[]) {
-        do {
-          array.sort(() => Math.random() - 0.5);
-        } while (array.every((piece, index) => piece.correctIndex === index));
-        return array;
-      },
-      /**
-       * Captures the index of the piece being dragged.
-       * @param index The index of the piece in the puzzlePieces array.
-       */
-      dragStart(index: number) {
-        this.dragIndex = index;
-      },
-      /**
-       * Handles dropping a piece onto another piece.
-       * Swaps the positions of the dragged and dropped pieces,
-       * updates the correctly placed count, and checks for completion.
-       * @param index The index of the drop target piece.
-       */
-      drop(index: number) {
-        if (this.dragIndex !== null) {
-          const draggedPiece = this.puzzlePieces[this.dragIndex];
-          const droppedPiece = this.puzzlePieces[index];
-
-          // Swap the pieces
-          this.puzzlePieces.splice(index, 1, draggedPiece);
-          this.puzzlePieces.splice(this.dragIndex, 1, droppedPiece);
-
-          // Reassign the array to itself to ensure reactivity
-          this.puzzlePieces = [...this.puzzlePieces];
-
-          // Update counter after drop
-          this.correctlyPlacedCount = this.puzzlePieces.filter((piece, i) => piece.correctIndex === i).length;
-
-          // Check for completion and redirect if completed
-          if (this.correctlyPlacedCount === this.totalPieces) {
-            setTimeout(() => this.redirect(), 750); // Timeout of 750ms before redirect
-          }
-        }
-      },
-      onResize() {
-        this.pieceSize = calculatePieceSize();
-        const pieces = this.preparePuzzlePieces();
-        pieces.forEach(piece => {
-          const puzzlePieceIndex = this.puzzlePieces.findIndex(puzzlePiece => puzzlePiece.correctIndex === piece.correctIndex);
-          this.puzzlePieces[puzzlePieceIndex].style = piece.style;
-        })
-      },
-    },
-    mounted() {
-      // Build puzzle
-      const pieces = this.preparePuzzlePieces();
-      this.totalPieces = pieces.length;
-      this.puzzlePieces = this.shuffleArray(pieces);
-
-      // Events
-      window.addEventListener('resize', this.onResize);
-    },
+/**
+ * Computes the style for the puzzle container grid.
+ *
+ * @returns The style object for the puzzle container.
+ */
+const puzzleContainerStyle = computed(() => {
+  return {
+    gridTemplateColumns: `repeat(${gridWidth}, ${pieceSize.value}px)`,
+    gridTemplateRows: `repeat(${gridHeight}, ${pieceSize.value}px)`,
   };
+});
+
+/**
+ * Prepares the puzzle pieces with their correct styles and indices.
+ *
+ * @returns An array of puzzle pieces.
+ */
+const preparePuzzlePieces = () => {
+  let pieces: PuzzlePiece[] = [];
+
+  for (let row = 0; row < gridHeight; row++) {
+    for (let col = 0; col < gridWidth; col++) {
+      const index = row * gridWidth + col;
+      pieces.push({
+        style: {
+          backgroundImage: 'url(/src/assets/404.svg)',
+          backgroundSize: `${gridWidth * pieceSize.value}px ${gridHeight * pieceSize.value}px`,
+          backgroundPosition: `-${col * pieceSize.value}px -${row * pieceSize.value}px`,
+          width: `${pieceSize.value}px`,
+          height: `${pieceSize.value}px`,
+        },
+        correctIndex: index,
+      });
+    }
+  }
+  return pieces;
+}
+
+/**
+ * Shuffles an array of puzzle pieces randomly, ensuring the puzzle is not already solved.
+ *
+ * @param array The array of puzzle pieces to shuffle.
+ * @returns The shuffled array of puzzle pieces.
+ */
+const shuffleArray = (array: PuzzlePiece[]) => {
+  do {
+    array.sort(() => Math.random() - 0.5);
+  } while (array.every((piece, index) => piece.correctIndex === index));
+  return array;
+}
+
+/**
+ * Handler for when a drag event starts.
+ *
+ * @param index The index of the puzzle piece being dragged.
+ */
+const dragStart = (index: number) => {
+  dragIndex.value = index;
+}
+
+/**
+ * Handler for when a drop event occurs.
+ *
+ * @param index The index of the puzzle piece being dropped on.
+ */
+const drop = (index: number) => {
+  if (dragIndex.value !== null) {
+    const draggedPiece = puzzlePieces[dragIndex.value];
+    const droppedPiece = puzzlePieces[index];
+
+    // Swap the pieces
+    puzzlePieces.splice(index, 1, draggedPiece);
+    puzzlePieces.splice(dragIndex.value, 1, droppedPiece);
+
+    // Update counter after drop
+    correctlyPlacedCount.value = puzzlePieces.filter((piece, i) => piece.correctIndex === i).length;
+
+    // Check for completion and redirect if completed
+    if (correctlyPlacedCount.value === totalPieces.value) {
+      setTimeout(() => router.push({ name: 'PortfolioView' }), 750); // Timeout of 750ms before redirect
+    }
+  }
+}
+
+/**
+ * Handler for when the window is resized.
+ * Recalculates the piece size and updates the puzzle pieces.
+ */
+const onResize = () => {
+  pieceSize.value = calculatePieceSize();
+  const pieces = preparePuzzlePieces();
+  pieces.forEach(piece => {
+    const puzzlePieceIndex = puzzlePieces.findIndex(puzzlePiece => puzzlePiece.correctIndex === piece.correctIndex);
+    puzzlePieces[puzzlePieceIndex].style = piece.style;
+  });
+}
+
+onMounted(() => {
+  // Build puzzle
+  const pieces = preparePuzzlePieces();
+  totalPieces.value = pieces.length;
+  puzzlePieces.push(...shuffleArray(pieces));
+  console.log(totalPieces, puzzlePieces);
+
+  // Events
+  window.addEventListener('resize', onResize);
+});
 </script>
 
 <template>
@@ -162,7 +167,7 @@
           <span class="puzzle-score">{{ correctlyPlacedCount }}/{{ totalPieces }}</span>
         </p>
       </div>
-      <button @click="redirect" class="btn text-button">
+      <button @click="router.push({ name: 'PortfolioView' })" class="btn text-button">
         Click to crack the code
       </button>
     </div>
