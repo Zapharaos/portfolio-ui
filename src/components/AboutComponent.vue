b828df640cad3867ee397147f335b9a8c46c1c1e
Zapharaos <matthieu.freitag@gmail.com>
2024-07-14 11:16:00 +0200

    feat: about section with drawings and credits

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -12,8 +12,42 @@ const props = defineProps<{
     <div class="container presentation">
       <h2 class="section-title">About</h2>
       <div class="section-content grid">
-        <img :src="about.image.file" :alt="about.image.name"/>
-        <p>
+        <div class="image-container">
+          <span
+              v-if="about.image.creditsUrl"
+              class="caption credits"
+          >
+            Credits :
+            <a :href="about.image.creditsUrl" type="_blank">
+              {{ about.image.creditsShortUrl ? about.image.creditsShortUrl : about.image.creditsUrl }}
+            </a>
+          </span>
+          <img
+              :src="about.image.file"
+              :alt="about.image.name"
+              class="about-image"
+          />
+        </div>
+        <div
+            v-if="about.imageResponsive"
+            class="responsive-image-container"
+        >
+          <img
+              :src="about.imageResponsive.file"
+              :alt="about.image.name"
+              class="about-image-responsive"
+          />
+          <span
+              v-if="about.imageResponsive.creditsUrl"
+              class="caption credits"
+          >
+            Credits :
+            <a :href="about.imageResponsive.creditsUrl" type="_blank">
+              {{ about.imageResponsive.creditsShortUrl ? about.imageResponsive.creditsShortUrl : about.imageResponsive.creditsUrl }}
+            </a>
+          </span>
+        </div>
+        <p class="about-text">
           {{ about.description }}
         </p>
       </div>
@@ -26,14 +60,49 @@ const props = defineProps<{
   grid-template-columns: 1fr 2fr;
   gap: 5rem;
 }
-img {
-  border-radius: 1rem;
-  width: inherit;
-  max-width: 350px;
+
+/* Image containers */
+.image-container, .responsive-image-container {
+  max-width: clamp(250px, 20vw, 300px);
 }
-p {
+.image-container {
+  display: flex;
+}
+.responsive-image-container {
+  display: none;
+}
+img {
+  max-width: 100%;
+}
+
+/* Credits */
+.credits {
+  text-align: center;
+  margin-top: 0.5rem;
+}
+.credits a {
+  color: var(--color-alternative);
+}
+
+/* Credits in desktop mode */
+.image-container .credits {
+  opacity: 0;
+  writing-mode: vertical-rl;
+  transform: rotate(-180deg);
+  transition: all 0.5s ease-in-out;
+}
+.image-container:hover {
+  cursor: pointer;
+}
+.image-container:hover .credits {
+  opacity: 1;
+}
+
+/* About me text */
+.about-text {
   text-align: justify;
   align-self: center;
+  line-height: 1.9;
 }
 @media (max-width: 992px) {
   .grid {
@@ -42,9 +111,15 @@ p {
     gap: 2rem;
     align-items: center;
   }
-  img {
-    width: auto;
-    max-width: 350px;
+  .image-container {
+    display: none;
+  }
+  .responsive-image-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .about-text {
+    line-height: var(--text-line-height);
   }
 }
 </style>
